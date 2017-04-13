7d580cb608ce6aa2d16d3a8c24ddd6b7fe448bb9
Ivan Egorov <vany.egorov@gmail.com>
2017-04-13 10:39:51 +0000

    use byte slice for code/decoder by default;

--- codec/decoder/decoder.go
+++ codec/decoder/decoder.go
@@ -12,7 +12,11 @@ import (
 	"github.com/BradburyLab/go/codec"
 )
 
-func Do(buf *bytes.Buffer, it interface{}, kind codec.Kind) error {
+func DoBuf(buf *bytes.Buffer, it interface{}, kind codec.Kind) error {
+	return Do(buf.Bytes(), it, kind)
+}
+
+func Do(buf []byte, it interface{}, kind codec.Kind) error {
 	switch kind {
 	case codec.JSON:
 		return JSON(buf, it)
@@ -27,14 +31,14 @@ func Do(buf *bytes.Buffer, it interface{}, kind codec.Kind) error {
 	return fmt.Errorf("failed to unmarshal %s: unsupported extension", kind)
 }
 
-func JSON(buf *bytes.Buffer, it interface{}) error { return json.Unmarshal(buf.Bytes(), it) }
-func YAML(buf *bytes.Buffer, it interface{}) error { return yaml.Unmarshal(buf.Bytes(), it) }
-func XML(buf *bytes.Buffer, it interface{}) error  { return xml.Unmarshal(buf.Bytes(), it) }
+func JSON(buf []byte, it interface{}) error { return json.Unmarshal(buf, it) }
+func YAML(buf []byte, it interface{}) error { return yaml.Unmarshal(buf, it) }
+func XML(buf []byte, it interface{}) error  { return xml.Unmarshal(buf, it) }
 
-func PROTO(buf *bytes.Buffer, it interface{}) error {
+func PROTO(buf []byte, it interface{}) error {
 	v, ok := it.(proto.Message)
 	if !ok {
 		return codec.ErrItNotProtobufable
 	}
-	return proto.Unmarshal(buf.Bytes(), v)
+	return proto.Unmarshal(buf, v)
 }
