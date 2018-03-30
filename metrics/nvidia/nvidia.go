608bda7cad10d163a4a544b180191ca5a1bf7d34
Ivan Egorov <vany.egorov@gmail.com>
2018-03-30 13:24:44 +0300

    set http-client transport settings for nvidia-api client;

--- metrics/nvidia/nvidia.go
+++ metrics/nvidia/nvidia.go
@@ -1,10 +1,12 @@
 package nvidia
 
 import (
+	"crypto/tls"
 	"net"
 	"net/http"
 	"net/url"
 	"strconv"
+	"time"
 )
 
 type Nvidia struct {
@@ -13,7 +15,14 @@ type Nvidia struct {
 	port   int
 	path   string
 
-	client *http.Client
+	timeout                   time.Duration
+	maxIdleConnectionsPerHost int
+	dialTimeout               time.Duration
+	dialKeepAlive             time.Duration
+	tlsHandshakeTimeout       time.Duration
+	tlsInsecureSkipVerify     bool
+
+	c *http.Client
 }
 
 func (it *Nvidia) Scheme() string { return it.scheme }
@@ -21,10 +30,37 @@ func (it *Nvidia) Host() string   { return it.host }
 func (it *Nvidia) Port() int      { return it.port }
 func (it *Nvidia) Path() string   { return it.path }
 
-func (it *Nvidia) SetScheme(v string) *Nvidia { it.scheme = v; return it }
-func (it *Nvidia) SetPath(v string) *Nvidia   { it.path = v; return it }
+func (it *Nvidia) SetScheme(v string) *Nvidia             { it.scheme = v; return it }
+func (it *Nvidia) SetPath(v string) *Nvidia               { it.path = v; return it }
+func (it *Nvidia) SetTimeout(v time.Duration)             { it.timeout = v }
+func (it *Nvidia) SetMaxIdleConnectionsPerHost(v int)     { it.maxIdleConnectionsPerHost = v }
+func (it *Nvidia) SetDialTimeout(v time.Duration)         { it.dialTimeout = v }
+func (it *Nvidia) SetDialKeepAlive(v time.Duration)       { it.dialKeepAlive = v }
+func (it *Nvidia) SetTLSHandshakeTimeout(v time.Duration) { it.tlsHandshakeTimeout = v }
+func (it *Nvidia) SetTLSInsecureSkipVerify(v bool)        { it.tlsInsecureSkipVerify = v }
 
 func (it *Nvidia) Addr() string { return net.JoinHostPort(it.host, strconv.Itoa(it.port)) }
+func (it *Nvidia) client() *http.Client {
+	if it.c == nil {
+		transport := &http.Transport{
+			MaxIdleConnsPerHost: it.maxIdleConnectionsPerHost,
+			Dial: (&net.Dialer{
+				Timeout:   it.dialTimeout,
+				KeepAlive: it.dialKeepAlive,
+			}).Dial,
+			TLSHandshakeTimeout: it.tlsHandshakeTimeout,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: it.tlsInsecureSkipVerify,
+			},
+		}
+		it.c = &http.Client{
+			Transport: transport,
+			Timeout:   it.timeout,
+		}
+	}
+
+	return it.c
+}
 
 func (it *Nvidia) Status() (Devices, error) {
 	u := url.URL{
@@ -32,7 +68,7 @@ func (it *Nvidia) Status() (Devices, error) {
 		Host:   it.Addr(),
 		Path:   it.Path(),
 	}
-	resp, e := it.client.Get(u.String())
+	resp, e := it.client().Get(u.String())
 	if e != nil {
 		return nil, Errorf(ERROR_1, u.String(), e.Error())
 	}
@@ -47,7 +83,12 @@ func NewNvidia(host string, port int) *Nvidia {
 	it.port = port
 	it.path = "/"
 
-	it.client = new(http.Client)
+	it.timeout = 1 * time.Second
+	it.maxIdleConnectionsPerHost = 16
+	it.dialTimeout = 1 * time.Second
+	it.dialKeepAlive = 10 * time.Second
+	it.tlsHandshakeTimeout = time.Second
+	it.tlsInsecureSkipVerify = true
 
 	return it
 }
